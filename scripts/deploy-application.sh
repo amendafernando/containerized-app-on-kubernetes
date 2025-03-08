13ddf23c12512b7cd9b5f0f20b58edf9dfced1e4
amendafernando <mail2amenda@gmail.com>
2025-03-08 00:57:17 -0500

    port update

--- scripts/deploy-application.sh
+++ scripts/deploy-application.sh
@@ -7,12 +7,24 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
 kubectl apply -f k8s-manifests/namespaces/
 
 # Deploy MySQL
+kubectl create secret docker-registry ecr-secret \
+  --docker-server=740165692460.dkr.ecr.us-east-1.amazonaws.com \
+  --docker-username=AWS \
+  --docker-password=$(aws ecr get-login-password --region us-east-1) \
+  -n mysql-namespace
+
 kubectl apply -f k8s-manifests/pods/mysql-pod.yaml
 kubectl apply -f k8s-manifests/replicasets/mysql-replicaset.yaml
 kubectl apply -f k8s-manifests/deployments/mysql-deployment.yaml
 kubectl apply -f k8s-manifests/services/mysql-service.yaml
 
 # Deploy Web Application
+kubectl create secret docker-registry ecr-secret \
+  --docker-server=740165692460.dkr.ecr.us-east-1.amazonaws.com \
+  --docker-username=AWS \
+  --docker-password=$(aws ecr get-login-password --region us-east-1) \
+  -n webapp-namespace
+
 kubectl apply -f k8s-manifests/pods/webapp-pod.yaml
 kubectl apply -f k8s-manifests/replicasets/webapp-replicaset.yaml
 kubectl apply -f k8s-manifests/deployments/webapp-deployment.yaml
